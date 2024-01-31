872a2af6a23ad8e925e1e5dd69ef0752c8bcd426
Lucas Radke <lucas.radke@mstc.edu>
2024-01-30 22:44:19 -0600

    6: Added model with getters and setters for patient creation. Added temporary link and view to display information.

--- EHRApplication/EHRApplication/Controllers/HomeController.cs
+++ EHRApplication/EHRApplication/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace EHRApplication.Controllers
             return View();
         }
 
+        public IActionResult Patient()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
