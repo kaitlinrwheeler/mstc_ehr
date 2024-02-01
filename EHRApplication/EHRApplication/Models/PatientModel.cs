4986851e6aff32f996d3a41f2f10f73b58138ba0
Lucas Radke <lucas.radke@mstc.edu>
2024-01-31 20:19:55 -0600

    6: Created form for patient demographic table. Added styling.

--- EHRApplication/EHRApplication/Models/PatientModel.cs
+++ EHRApplication/EHRApplication/Models/PatientModel.cs
@@ -1,21 +1,8 @@
 ﻿namespace EHRApplication.Models
 {
-    public class PatientFormModel
+    public class PatientModel
     {
-        public int MHN { get; set; }
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
-        public string Suffix { get; set; }
-        public DateTime DOB { get; set; }
-        public string Gender { get; set; }
-        public string PreferredLanguage { get; set; }
-        public string Ethnicity { get; set; }
-        public string Race { get; set; }
-        public string Religion { get; set; }
-        public int PrimaryPhysician { get; set; }
-        public string LegalGuardian1 { get; set; }
-        public string LegalGuardian2 { get; set; }
+        
     }
 
 }
