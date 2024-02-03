64e547ccd46c5d8595362a8cf76e61b0a412442f
Jordan Shostak <17109533@mstc.edu>
2024-02-02 19:21:52 -0600

    #9 Added dark mode.

--- EHRApplication/EHRApplication/wwwroot/js/site.js
+++ EHRApplication/EHRApplication/wwwroot/js/site.js
@@ -68,6 +68,57 @@ function initMenu() {
         }
     );
 }
-$(document).ready(function () {
+$(function() {
     initMenu();
 });
+
+
+
+
+
+//const themeSwitch = document.getElementById('theme-switch');
+
+//themeSwitch.addEventListener('change', function () {
+//    let htmlClasses = document.documentElement.classList;
+//    if (this.checked) {
+//        htmlClasses.add('dark');
+//        localStorage.setItem('theme', 'dark'); //we are using this to remember the user choice
+//    } else {
+//        htmlClasses.remove('dark');
+//        localStorage.setItem('theme', 'light');
+//    }
+//});
+
+
+//Loads when page is loaded.
+//Gets the theme from local storage and set it to the data-bs-theme. Needed for when the user refreshes the page.
+$(function () {
+    const savedTheme = localStorage.getItem('theme') || 'light';
+
+    var element = document.body;
+
+    element.dataset.bsTheme = savedTheme;
+
+    //Set the dark mode switch to the proper direction based on the selected theme.
+    if (savedTheme === 'dark') {
+        $('#flexSwitchCheckChecked').prop('checked', true);
+    }
+});
+
+
+//Switches the theme. Tied the the dark mode switch.
+function darkSwitch() {
+    //Gets the body element so we can check the the bs-theme is and work with it.
+    var element = document.body;
+
+    //Determines what the new theme is. It flip flops.
+    var newTheme = element.dataset.bsTheme == "light" ? "dark" : "light";
+
+    //Sets the body's bs-theme to the new theme, i.e. light or dark.
+    element.dataset.bsTheme = newTheme;
+
+    //Sets the local storage theme to the new theme. Needed for if a user refreshes the page, so the theme doesn't revert back to it's default.
+    localStorage.setItem('theme', newTheme);
+
+     
+}
---
+++ EHRApplication/EHRApplication/wwwroot/js/themeSwitcher.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    const savedTheme = localStorage.getItem('theme') || 'light';
+
+    var element = document.body;
+
+    element.dataset.bsTheme = savedTheme;
+});
+
+
+//Switches the theme. Tied the the dark mode switch.
+function darkSwitch() {
+    var element = document.body;
+    element.dataset.bsTheme = element.dataset.bsTheme == "light" ? "dark" : "light";
+}
