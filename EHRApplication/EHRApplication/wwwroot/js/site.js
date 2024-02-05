a3cbe8e08b418fe70f7f5a6cc077b3b4a5eba08d
Jordan Shostak <17109533@mstc.edu>
2024-02-05 11:54:44 -0600

    #15 Changed form error colors. Got rid of old comments. Adjust patient form header font size.

--- EHRApplication/EHRApplication/wwwroot/js/site.js
+++ EHRApplication/EHRApplication/wwwroot/js/site.js
@@ -52,26 +52,6 @@ function initMenu() {
 
 
 // Gets the theme from local storage and set it to the data-bs-theme. Needed for when the user refreshes the page.
-//function getPreviouslySetTheme() {
-
-//    // Gets the theme previously seleted theme from local storage.
-//    const savedTheme = localStorage.getItem('theme') || 'light';
-
-//    // Gets the body element. This might need to become a list of elements once we add pages, not entirely sure how dark mode will be working with all the content planned.
-//    var element = document.body;
-
-//    // Sets the element's theme to the previously saved theme.
-//    element.dataset.bsTheme = savedTheme;
-
-//    // Set the dark mode switch to the proper direction based on the selected theme.
-//    if (savedTheme === 'dark') {
-//        $('#flexSwitchCheckChecked').prop('checked', true);
-//    }
-//}
-
-
-
-// Gets the theme from local storage and set it to the data-bs-theme. Needed for when the user refreshes the page.
 function getPreviouslySetTheme() {
 
     // Gets the theme previously selected theme from local storage.
@@ -91,23 +71,6 @@ function getPreviouslySetTheme() {
         $('#flexSwitchCheckChecked').prop('checked', true);
     }
 }
-
-
-
-// Switches the theme. Tied the the dark mode switch.
-//function darkSwitch() {
-//    // Gets the body element so we can check the the bs-theme is and work with it.
-//    var element = document.body;
-
-//    // Determines what the new theme is. It flip flops.
-//    var newTheme = element.dataset.bsTheme == "light" ? "dark" : "light";
-
-//    // Sets the body's bs-theme to the new theme, i.e. light or dark.
-//    element.dataset.bsTheme = newTheme;
-
-//    // Sets the local storage theme to the new theme. Needed for if a user refreshes the page, so the theme doesn't revert back to it's default.
-//    localStorage.setItem('theme', newTheme);
-//}
 
 
 // Switches the theme. Tied to the dark mode switch.
