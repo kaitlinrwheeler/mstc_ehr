d3aaef03ac1653a1650e3f1874fbed64012f73d2
Josh Halat <17398978@mstc.edu>
2024-02-06 23:48:28 -0600

    Created the register page based off the login page

---
+++ EHRApplication/EHRApplication/Controllers/Login_Register.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EHRApplication.Controllers
+{
+    public class Login_Register : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
