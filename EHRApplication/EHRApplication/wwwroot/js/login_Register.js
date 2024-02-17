545979c9a60ac4da4f8650fea2f736172a5fa367
Josh Halat <17398978@mstc.edu>
2024-02-17 12:55:12 -0600

    Added the password requirement popout

--- EHRApplication/EHRApplication/wwwroot/js/login_Register.js
+++ EHRApplication/EHRApplication/wwwroot/js/login_Register.js
@@ -1,38 +1,31 @@
-﻿// Click event for the question mark icon
-$('.icon').click(function (e) {
-    $('.icon.active').removeClass('active');
-    $(this).addClass('active');
-    e.preventDefault();
-    e.stopPropagation();
+﻿var requirementsVisible = false;
+var hoveringPopup = false;
+
+function showRequirements() {
+    document.getElementById("requirementsPopup").style.display = "block";
+}
+
+function hideRequirements() {
+    if (!requirementsVisible && !hoveringPopup) {
+        document.getElementById("requirementsPopup").style.display = "none";
+    }
+}
+
+document.getElementById("requirementsPopup").addEventListener("mouseover", function () {
+    hoveringPopup = true;
 });
 
-// Click event for the document to remove active class
-$(document).click(() => {
-    $('.icon.active').removeClass('active');
+document.getElementById("requirementsPopup").addEventListener("mouseout", function () {
+    hoveringPopup = false;
 });
 
-// Joyride functionality
-let joyridePos = 0;
-const joyride = ((config) => {
-    const tester = () => {
-        $('body').attr('joyride-step', joyridePos);
-        const step = config[joyridePos];
-        const el = $(step[0]);
-        const check = () => {
-            if (step[2].bind(el[0])(el[0])) {
-                el[0][`on${step[1]}`] = undefined;
-                joyridePos++;
-                tester();
-            }
-        };
-        el[0][`on${step[1]}`] = check;
-    };
-    tester();
-})([
-    ['.text textarea', 'click', el => true],
-    ['.text textarea', 'keydown', el => el.value.length > 10],
-    ['.icon .fa-facebook', 'click', el => true],
-    ['.yes-btns', 'click', el => true],
-    ['.promote-btns', 'click', el => true],
-    // Add more steps as needed
-]);
+document.addEventListener("click", function (event) {
+    var requirementsPopup = document.getElementById("requirementsPopup");
+    var questionIcon = document.getElementById("questionIcon");
+    var targetElement = event.target;
+
+    if (requirementsVisible && targetElement !== requirementsPopup && targetElement !== questionIcon) {
+        hideRequirements();
+        requirementsVisible = false;
+    }
+});
