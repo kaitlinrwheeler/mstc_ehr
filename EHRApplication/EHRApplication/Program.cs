0e4a00dea99efa9ee04fa399cb2ae88a4b82ef08
Kaitlin Wheeler <kaitwheel@gmail.com>
2024-02-17 04:18:00 -0600

    landing page - fixed content and background positioning, updated nav bar and button links (registration and resource controllers aren't on this branch).
    removed dark mode from this view. no user dash/patient list for redirection yet so user can get to login, registration, or resources from this view.

--- EHRApplication/EHRApplication/Program.cs
+++ EHRApplication/EHRApplication/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=LandingPage}/{id?}");
+    pattern: "{controller=Home}/{action=LandingPage}/{id?}"); // changed from action=Index to action=LandingPage
 
 app.Run();
