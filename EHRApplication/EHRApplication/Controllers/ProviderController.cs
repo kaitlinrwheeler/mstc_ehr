d40c69f06e5d0050e6885b9ac3f55fbf8ce5f1b6
Lucas Radke <lucas.radke@mstc.edu>
2024-02-20 20:22:10 -0600

    59: Added provider controller and view to display the providers.

---
+++ EHRApplication/EHRApplication/Controllers/ProviderController.cs
@@ -0,0 +1,58 @@
+﻿using EHRApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace EHRApplication.Controllers
+{
+    public class ProviderController : Controller
+    {
+        private readonly LogService _logService;
+
+        private readonly string _connectionString;
+
+        public ProviderController(LogService logService, IConfiguration configuration)
+        {
+            _logService = logService;
+            _connectionString = configuration["ConnectionStrings:DefaultConnection"];
+        }
+
+        /// <summary>
+        /// Displays the view that shows all the providers from the database
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult AllProviders()
+        {
+            List<Providers> allProviders = new List<Providers>();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                //query to grab the providerID, firstname, lastname, and specialty
+                string sql = "SELECT providerId, firstName, lastName, specialty FROM [dbo].[Providers] ORDER BY providerId ASC";
+
+                SqlCommand cmd = new SqlCommand(sql, connection);
+
+                using (SqlDataReader dataReader = cmd.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        Providers provider = new Providers
+                        {
+                            providerId = Convert.ToInt32(dataReader["providerId"]),
+                            firstName = Convert.ToString(dataReader["firstName"]),
+                            lastName = Convert.ToString(dataReader["lastName"]),
+                            specialty = Convert.ToString(dataReader["specialty"])
+                        };
+
+                        allProviders.Add(provider);
+                    }
+                }
+
+                connection.Close();
+            }
+
+            return View(allProviders);
+        }
+    }
+}
