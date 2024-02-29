e6e652d9cf8234187a5178f3ed3badca716457f2
Josh Halat <17398978@mstc.edu>
2024-02-28 20:23:25 -0600

    got the asp validation working. waiting for some changes made in branch 57 to finish the on blur validation.

---
+++ EHRApplication/EHRApplication/Controllers/ContactController.cs
@@ -0,0 +1,23 @@
+﻿using EHRApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EHRApplication.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(PatientContact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(contact);
+            }
+            return View();
+        }
+    }
+}
--- EHRApplication/EHRApplication/Models/PatientContact.cs
+++ EHRApplication/EHRApplication/Models/PatientContact.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EHRApplication.Models
@@ -13,24 +14,38 @@ namespace EHRApplication.Models
 
         public int MHN { get; set; }
 
+        [Required(ErrorMessage = "Address is required")]
+        [RegularExpression(@"^[a-zA-Z0-9\s\/\-]*$", ErrorMessage = "Please enter alphabetic characters only.")]
         public string address { get; set; }
 
+        [Required(ErrorMessage = "City is required")]
+        [RegularExpression(@"^[a-zA-Z\s\/\-]*$", ErrorMessage = "Please enter alphabetic characters only.")]
         public string city { get; set; }
-        
+
+        [Required(ErrorMessage = "State is required")]
+        [RegularExpression(@"^[a-zA-Z\s\/\-]*$", ErrorMessage = "Please enter alphabetic characters only.")]
         public string state { get; set; }
 
-        public int zipcode { get; set; }
+        [Required(ErrorMessage = "Zipcode is required")]
+        [RegularExpression(@"^\d{5}(-\d{4})?$", ErrorMessage = "Invalid Zipcode")]
+        public int? zipcode { get; set; }
 
+        [RegularExpression(@"^[0-9\s\+\-]*$", ErrorMessage = "Please enter numbers only.")]
         public string phone { get; set; }
 
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string email { get; set; }
 
-        public string ECFirstName { get; set; }
+        [RegularExpression(@"^[a-zA-Z\s\/\-]*$", ErrorMessage = "Please enter alphabetic characters only.")]
+        public string? ECFirstName { get; set; }
 
-        public string ECLastName { get; set; }
+        [RegularExpression(@"^[a-zA-Z\s\/\-]*$", ErrorMessage = "Please enter alphabetic characters only.")]
+        public string? ECLastName { get; set; }
 
-        public string ECRelationship { get; set; }
+        [RegularExpression(@"^[a-zA-Z\s\/\-]*$", ErrorMessage = "Please enter alphabetic characters only.")]
+        public string? ECRelationship { get; set; }
 
-        public string ECPhone { get; set; }
+        [RegularExpression(@"^[0-9\s\+\-]*$", ErrorMessage = "Please enter numbers only.")]
+        public string? ECPhone { get; set; }
     }
 }
