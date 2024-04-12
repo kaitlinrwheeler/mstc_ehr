3d4517d8286d7f43ef62fef4cdb72cdb2f2442f7
Kaitlin Wheeler <kaitwheel@gmail.com>
2024-04-12 11:36:17 -0500

    WIP

--- EHRApplication/EHRApplication/Controllers/ProvidersController.cs
+++ EHRApplication/EHRApplication/Controllers/ProvidersController.cs
@@ -2,6 +2,7 @@
 using EHRApplication.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace EHRApplication.Controllers
 {
@@ -57,6 +58,104 @@ namespace EHRApplication.Controllers
             }
 
             return View(allProviders);
+        }
+
+        public IActionResult CreateProvider(int providerId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateProvider(Providers provider)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(provider);
+            }
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string sql = "INSERT INTO [Providers] (firstName, lastName, specialty) VALUES (@firstName, @lastName, @specialty)";
+
+                using (SqlCommand cmd = new SqlCommand(sql, connection))
+                {
+                    cmd.CommandType = CommandType.Text;
+
+                    cmd.Parameters.AddWithValue("@firstName", provider.firstName);
+                    cmd.Parameters.AddWithValue("@lastName", provider.lastName);
+                    cmd.Parameters.AddWithValue("@specialty", provider.specialty);
+
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    connection.Close();
+                }
+            }
+            return View();
+        }
+
+        public IActionResult EditProvider(int providerId)
+        {
+            Providers provider = new Providers();
+
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+
+                string sql = "SELECT providerId, firstName, lastName, specialty FROM [dbo].[Providers] WHERE providerId = @providerId";
+
+                SqlCommand cmd = new SqlCommand(sql, connection);
+                cmd.Parameters.AddWithValue("@providerId", providerId);
+
+                using (SqlDataReader dataReader = cmd.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        provider.providerId = Convert.ToInt32(dataReader["providerId"]);
+                        provider.firstName = Convert.ToString(dataReader["firstName"]);
+                        provider.lastName = Convert.ToString(dataReader["lastName"]);
+                        provider.specialty = Convert.ToString(dataReader["specialty"]);
+                    }
+                }
+
+                connection.Close();
+            }
+
+            return View(provider);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateProviderActiveStatus(int providerId, bool activeStatus)
+        {
+            using (SqlConnection connection = new SqlConnection(this._connectionString))
+            {
+                connection.Open();
+
+                // Sql query.
+                string sql = "UPDATE [dbo].[Providers] SET Active = @active WHERE providerId = @providerId";
+
+                SqlCommand cmd = new SqlCommand(sql, connection);
+
+                // Replace placeholder with parameter to avoid SQL injection.
+                cmd.Parameters.AddWithValue("@providerId", providerId);
+                cmd.Parameters.AddWithValue("@active", activeStatus);
+
+                // Execute the SQL command.
+                int rowsAffected = cmd.ExecuteNonQuery();
+
+                connection.Close();
+
+                // Check if any rows were affected.
+                if (rowsAffected > 0)
+                {
+                    // Successfully updated.
+                    return Ok("Successfully updated.");
+                }
+                else
+                {
+                    // No rows were affected, return an error message.
+                    return BadRequest("Error, please try again.");
+                }
+            }
         }
     }
 }
