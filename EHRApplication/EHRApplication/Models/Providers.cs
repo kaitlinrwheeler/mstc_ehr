e019758807dcc9f425c4a1fecc2314331f9b33f0
Kaitlin Wheeler <kaitwheel@gmail.com>
2024-04-22 19:51:24 -0500

    fixed validation

--- EHRApplication/EHRApplication/Models/Providers.cs
+++ EHRApplication/EHRApplication/Models/Providers.cs
@@ -7,10 +7,16 @@ namespace EHRApplication.Models
         [Key]
         public int providerId { get; set; }
 
+        [Required(ErrorMessage = "Please enter provider's first name.")]
+        [RegularExpression(@"^[a-zA-Z\s\/\-'']*$", ErrorMessage = "Please enter alphabetic characters only.")]
         public string firstName { get; set; }
 
+        [Required(ErrorMessage = "Please enter provider's last name.")]
+        [RegularExpression(@"^[a-zA-Z\s\/\-'']*$", ErrorMessage = "Please enter alphabetic characters only.")]
         public string lastName { get; set; }
 
+        [Required(ErrorMessage = "Please enter provider's specialty.")]
+        [RegularExpression(@"^[a-zA-Z\s\/\-'']*$", ErrorMessage = "Please enter alphabetic characters only.")]
         public string specialty { get; set; }
 
         public bool active { get; set; }
