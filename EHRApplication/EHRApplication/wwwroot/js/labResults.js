b0a58b0616b9bc70b937af4718b5f1842d9bdd43
Jordan Shostak <17109533@mstc.edu>
2024-05-10 12:08:58 -0500

    Added delete buttons to views, delete actions, and js to handle visual deletion for lab orders and lab results.

---
+++ EHRApplication/EHRApplication/wwwroot/js/labResults.js
@@ -0,0 +1,37 @@
+﻿
+// Adds event listener to delete button to delete ins;urance record without having to refresh the page.
+document.addEventListener('DOMContentLoaded', function () {
+    const deleteButtons = document.querySelectorAll('.delete-button');
+    const modal = new bootstrap.Modal(document.getElementById('deleteConfirmModal'));
+    const confirmDeleteBtn = document.getElementById('confirmDeleteBtn');
+    let labId;
+
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', function (event) {
+            event.preventDefault(); // Prevent the default form submission
+            labId = this.getAttribute('data-labId');
+            message = this.getAttribute('data-message'); // Get the name
+            document.getElementById('messagePlaceholder').textContent = message; // Update the name placeholder
+            modal.show();
+        });
+    });
+
+    confirmDeleteBtn.addEventListener('click', function () {
+        fetch(`/Lab/DeleteLabResults?resultId=${labId}`, {
+            method: 'POST',
+        })
+            .then(response => {
+                if (response.ok) {
+                    // Row to remove
+                    const row = document.getElementById(`row-${labId}`);
+                    row.remove();
+                }
+                else {
+                    alert('Failed to delete. Please try again.');
+                }
+            })
+            .finally(() => {
+                modal.hide();
+            });
+    });
+});
